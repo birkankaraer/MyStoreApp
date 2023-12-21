28f3b7b11cc29e50bb7ded167bae5aa38ae5522b
Birkankaraer <karaermustafabirkan@gmail.com>
2023-12-21 22:33:14 +0300

    Layout

---
+++ Migrations/20231219133715_ProductSeedData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.Migrations
+{
+    public partial class ProductSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<RepositoryContext>(options=>{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
